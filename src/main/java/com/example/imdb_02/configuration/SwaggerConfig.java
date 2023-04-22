de23a3bec143799e71b6b71ae64f6087acd01a6d
gega <latariagega1@gmail.com>
2023-04-22 14:23:50 +0400

    updated swagger config

--- src/main/java/com/example/imdb_02/configuration/SwaggerConfig.java
+++ src/main/java/com/example/imdb_02/configuration/SwaggerConfig.java
@@ -13,12 +13,8 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("SpringShop API")
-                        .description("Spring shop sample application")
-                        .version("v0.0.1")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("SpringShop Wiki Documentation")
-                        .url("https://springshop.wiki.github.org/docs"));
+                .info(new Info().title("MovieApp API")
+                        .description("Movie app swagger")
+                        .version("v0.0.1"));
     }
 }
